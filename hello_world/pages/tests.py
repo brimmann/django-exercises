e9c5b2e8312da3827aa5949317b87e2d906d1715
Mohammad Rashid <rashidstanikzai.net@gmail.com>
2021-06-07 15:13:56 +0500

    test cases sccesseded

--- hello_world/pages/tests.py
+++ hello_world/pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+#pylint: disable=unused-variable
 
-# Create your tests here.
+from django.test import SimpleTestCase
+
+class SimpleTest(SimpleTestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_aboutpage_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+        
